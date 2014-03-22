3ba0d5cdad0df749c1e5526fcf567a13f51eb090
Brandon Shackelford <brandon@bentlyenterprises.com>
2014-03-21 20:15:16 -0700

    Update gitignore for source map files, update gulpfile and node packages to include js minify

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,11 @@ var gulp = require('gulp'),
     prefix = require('gulp-autoprefixer'),
     minifyCSS = require('gulp-minify-css'),
     sass = require('gulp-ruby-sass'),
+    minifyJS = require('gulp-jsmin'),
+    rename = require('gulp-rename')
     imagemin = require('gulp-imagemin'),
     svgmin = require('gulp-svgmin'),
+    jshint = require('gulp-jshint'),
     csslint = require('gulp-csslint');
 
 // Task to minify all css files in the css directory
@@ -18,6 +21,14 @@ gulp.task('minify-css', function(){
     gulp.src('./css/*.css')
         .pipe(minifyCSS({keepSpecialComments: 0}))
         .pipe(gulp.dest('./css/'));
+});
+
+// Task to minify all css files in the css directory
+gulp.task('minify-js', function () {
+    gulp.src('./js/*.js')
+        .pipe(jsmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('min'));
 });
 
 // Reload html
@@ -42,6 +53,13 @@ gulp.task('minify-svg', function(){
         .pipe(gulp.dest('./img/svg'));
 });
 
+// Task to run jshint and pipe output to terminal
+gulp.task('jshint', function(){
+    gulp.src('./js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
 // Use csslint without box-sizing or compatible vendor prefixes (these
 // don't seem to be kept up to date on what to yell about)
 gulp.task('csslint', function(){
@@ -58,7 +76,7 @@ gulp.task('csslint', function(){
 gulp.task('pre-process', function(){
     gulp.src('./scss/style.scss')
         .pipe(watch(function(files) {
-            return files.pipe(sass({loadPath: ['./scss/'], style: "compact"}))
+            return files.pipe(sass({loadPath: ['./scss/'], sourcemap: true, style: "compact"}))
                 .pipe(prefix())
                 .pipe(gulp.dest('./css/'))
                 .pipe(livereload(server));
@@ -66,22 +84,22 @@ gulp.task('pre-process', function(){
 });
 
 /*
-     DEFAULT TASK
+    DEFAULT TASK
 
- • Process sass and lints outputted css
- • Outputted css is run through autoprefixer
- • Sends updates to any files in directory to browser for
-     automatic reloading
+    • Process sass and lints outputted css
+    • Outputted css is run through autoprefixer
+    • Sends updates to any files in directory to browser for
+        automatic reloading
 
 */
 gulp.task('default', function(){
     server.listen(35729, function (err) {
-        gulp.watch(['*.html', '*/*.html', './scss/*.scss'], function(event) {
+        gulp.watch(['*.html', '*/*.html', './scss/*.scss', './scss/*/*.scss', './js/*.js'], function(event) {
             gulp.run('pre-process', 'csslint', 'reload');
         });
     });
 });
 
-gulp.task('production', function(){
-    gulp.run('minify-css', 'minify-img', 'minify-svg');
+gulp.task('prod', function(){
+    gulp.run('minify-css', 'minify-js', 'minify-img', 'minify-svg');
 });
